22e3aa33c9cdc3ccfe76c9b596b7ace64d96d560
javi <javier.s408034@cesurformacion.com>
2025-01-27 00:34:01 +0100

    sesion4

--- Nivel1/Sesion4/js/functions.js
+++ Nivel1/Sesion4/js/functions.js
@@ -1,5 +1,3 @@
-//*************
-// AJAX request
 function procesaDatos() {
 
 
@@ -20,6 +18,22 @@ function init(){
         Zip: [20101 , 23019 , 20931 , 34012 , 10234 , 12041 , 13443]
     }
     
+    document.getElementById("inputEmail").value = data.email;
+    document.getElementById("inputPassword").value = data.password;
+    document.getElementById("inputAddress").value = data.adress;
+    document.getElementById("inputAddress2").value = data.adress2;
+    document.getElementById("inputCity").value = data.City;
+
+    
+    var indice = Math.ceil(Math.random() * data.Zip.length-1);
+    
+    zip = document.getElementById("inputZip");
+    var option = document.createElement("option");
+    option.value = data.Zip[indice];
+    option.textContent = data.Zip[indice];
+    zip.appendChild(option);
+
+   
     // Iniciar los elementos HTML.
     // ---------------------------
     // Asignar a cada input su correspondiente valor.
@@ -28,11 +42,10 @@ function init(){
 
 
     // Asignar al select de ZIP sus valores apoyandonos en data.zip
-    data.Zip.forEach(function(el){
+    //data.Zip.forEach(function(el){
         // Aqui construimos las cadenas <option value=""></option> que insertaremos en el select ZIP
         // Pista , haz en este bucle => console.log(el) para ver que sale por consola y tomar una idea de que hacer.
-    });
+
        
 }
-
 init();
